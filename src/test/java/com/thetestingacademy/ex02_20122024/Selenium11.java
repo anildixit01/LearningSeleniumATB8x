a04cf04598ff0916e1e62d9f7279413f01e34c51
anildixit01 <anildixit01@gmail.com>
2024-12-22 09:43:06 +0530

    lERNING SELENIUM ex01 and 02 commited

---
+++ src/test/java/com/thetestingacademy/ex02_20122024/Selenium11.java
@@ -0,0 +1,28 @@
+package com.thetestingacademy.ex02_20122024;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class Selenium11 {
+    @Test
+    public void testMethod01(){
+
+//            Chromium - Parent Project - OS
+//                - Chrome - Google (OS)
+//                - Edge
+//                - Opera
+//                - Brave Browser
+//                - TorX Browser
+//
+//            Mozilla - Firefox Project
+//
+//            Safari - Apple.com
+
+
+
+
+    }
+}
